640d9b6e7a4d8f9adfbe8d743fe03860197b31fd
Cosme Manuel Tejeda <cosme.m.tejeda@gmail.com>
2023-01-17 22:37:43 -0600

    Bat AI Start
    Added a particle effect for when an enemy is hit. Began developing the enemy AI. Created a player detection zone for the bat.

--- Project/Enemies/Bat.gd
+++ Project/Enemies/Bat.gd
@@ -2,16 +2,43 @@ extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
-const FRICTION = 200 # 200
-const KNOCKBACK_STRENGTH = 120 # 120
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+var state = CHASE
 
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
+export var KNOCKBACK_FRICTION = 200 # 200
+export var KNOCKBACK_STRENGTH = 120 # 120
+
+var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
 
 onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
 
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, KNOCKBACK_FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+			
+		WANDER:
+			pass
+			
+		CHASE:
+			pass
+
+func seek_player():
+	if playerDetectionZone.can_see_player(): # Calling down/ signaling up
+		state = CHASE
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage # Calling Down
